1fd9c32edd4981c4ffe23cb82b3a811ee1c159eb
Graham Leach-Krouse <gleachkr@gmail.com>
2023-08-05 18:30:33 -0400

    Basic exercise content chunking

--- filters/exercises.lua
+++ filters/exercises.lua
@@ -15,4 +15,45 @@ local function wrapper(opts, label, content)
     })
 end
 
-return wrapper
+local function lines(str)
+    local pos = 1;
+    return function()
+        if not pos then return nil end
+        local  p1, p2 = string.find(str, "\r?\n", pos)
+        local line
+        if p1 then
+            line = str:sub(pos, p1 - 1)
+            pos = p2 + 1
+        else
+            line = str:sub(pos)
+            pos = nil
+        end
+        return line
+    end
+end
+
+local function chunks(s)
+    local currentChunk = nil
+    local result = {}
+    for line in lines(s) do
+        if not string.match(s,"^|") then
+            if currentChunk then
+                table.insert(result,currentChunk)
+            end
+            currentChunk = {
+                label = string.gmatch(line,"%S*")(),
+                problem = string.gmatch(line,"%S*%s*(.*)")()
+            }
+        else
+            table.insert(currentChunk,line)
+        end
+    end
+    table.insert(result,currentChunk)
+    return result
+end
+
+
+return {
+    wrapper = wrapper,
+    chunks = chunks
+}
